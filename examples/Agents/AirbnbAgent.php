a58eaebaa4df100a9b5a42326007fa70137d771e
joris <joris@swis.nl>
2025-04-01 14:17:44 +0200

    feat: Use the Airbnb MCP as example for an MCP agent

--- examples/Agents/AirbnbAgent.php
+++ examples/Agents/AirbnbAgent.php
@@ -4,31 +4,28 @@ use Swis\Agents\Agent;
 use Swis\Agents\Mcp\McpConnection;
 use Swis\McpClient\Client;
 
-/**
- * For this example you need to install the math-mcp server.
- * npm install git@github.com:EthanHenrickson/math-mcp.git
- */
-class McpAgent
+class AirbnbAgent
 {
     public function __invoke(): Agent
     {
         return new Agent(
-            name: 'Calculator Agent',
-            description: 'This Agent can perform arithmetic operations.',
+            name: 'Airbnb Agent',
+            description: 'This Agent can search for Airbnb listings.',
+            instruction: 'Current date: ' . date('D j F Y - H:i'),
             mcpConnections: [
-                new MathMcpConnection(),
+                new AirbnbMcpConnection(),
             ]
         );
     }
 
 }
 
-class MathMcpConnection extends McpConnection
+class AirbnbMcpConnection extends McpConnection
 {
     public function __construct()
     {
         [$client, $process] = Client::withProcess(
-            command: 'node ' . realpath(__DIR__ . '/../../node_modules/math-mcp/build/index.js'),
+            command: 'npx -y @openbnb/mcp-server-airbnb --ignore-robots-txt',
             // PHP has the habit of terminating child processes when waiting for CLI user input
             // This option will restart the child process if it terminates unexpectedly
             autoRestartAmount: 99
@@ -36,7 +33,7 @@ class MathMcpConnection extends McpConnection
 
         parent::__construct(
             client: $client,
-            name: 'Math MCP',
+            name: 'Airbnb MCP',
         );
     }
 }

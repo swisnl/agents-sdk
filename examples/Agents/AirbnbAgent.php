8addf13eb858c1dd4284fc11f03510df01a54fc3
joris <joris@swis.nl>
2025-10-17 16:08:03 +0200

    Fix: Pass along the variable containing the process resource so it doesn't leave the scope along the way

--- examples/Agents/AirbnbAgent.php
+++ examples/Agents/AirbnbAgent.php
@@ -7,12 +7,14 @@ class AirbnbAgent
 {
     public function __invoke(): Agent
     {
+        [$connection, $process] = McpConnection::forProcess('npx -y @openbnb/mcp-server-airbnb --ignore-robots-txt', 99);
+
         return new Agent(
             name: 'Airbnb Agent',
             description: 'This Agent can search for Airbnb listings.',
             instruction: 'Current date: ' . date('D j F Y - H:i'),
             mcpConnections: [
-                McpConnection::forProcess('npx -y @openbnb/mcp-server-airbnb --ignore-robots-txt', 99)
+                $connection,
             ]
         );
     }
